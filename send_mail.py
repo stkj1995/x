a6496810221618f0c78be939cb4435ed41d85b12
Sophie <soph1155@stud.kea.dk>
2025-11-23 12:31:28 +0100

    first commit

---
+++ send_mail.py
@@ -0,0 +1,38 @@
+##############################
+def send_verify_email(to_email, user_verification_key):
+    try:
+        # Create a gmail fullflaskdemomail
+        # Enable (turn on) 2 step verification/factor in the google account manager
+        # Visit: https://myaccount.google.com/apppasswords
+        # Copy the key
+
+        # Email and password of the sender's Gmail account
+        sender_email = "YOUR GMAIL HERE"
+        password = "APP PASSWORD HERE"  # If 2FA is on, use an App Password instead
+
+        # Receiver email address
+        receiver_email = "YOUR GMAIL HERE"
+        
+        # Create the email message
+        message = MIMEMultipart()
+        message["From"] = "My company name"
+        message["To"] = receiver_email
+        message["Subject"] = "Please verify your account"
+
+        # Body of the email
+        body = f"""To verify your account, please <a href="http://127.0.0.1/verify/{user_verification_key}">click here</a>"""
+        message.attach(MIMEText(body, "html"))
+
+        # Connect to Gmail's SMTP server and send the email
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()  # Upgrade the connection to secure
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+        print("Email sent successfully!")
+
+        return "email sent"
+       
+    except Exception as ex:
+        raise_custom_exception("cannot send email", 500)
+    finally:
+        pass
---
+++ x.py
@@ -0,0 +1,170 @@
+from flask import request, make_response, render_template
+import mysql.connector
+import re 
+import dictionary
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from functools import wraps
+
+import json
+
+
+from icecream import ic
+ic.configureOutput(prefix=f'----- | ', includeContext=True)
+
+UPLOAD_ITEM_FOLDER = './images'
+
+##############################
+allowed_languages = ["english", "danish", "spanish"]
+google_spread_sheet_key = ""
+default_language = "english"
+
+def lans(key):
+    with open("dictionary.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data[key][default_language]
+
+##############################
+def db():
+    try:
+        db = mysql.connector.connect(
+            host = "mariadb",
+            user = "root",  
+            password = "password",
+            database = "x"
+        )
+        cursor = db.cursor(dictionary=True)
+        return db, cursor
+    except Exception as e:
+        print(e, flush=True)
+        raise Exception("Twitter exception - Database under maintenance", 500)
+
+
+##############################
+def no_cache(view):
+    @wraps(view)
+    def no_cache_view(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        return response
+    return no_cache_view
+
+
+##############################
+REGEX_EMAIL = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
+def validate_user_email(lan = "en"):
+    user_email = request.form.get("user_email", "").strip()
+    if not re.match(REGEX_EMAIL, user_email): raise Exception(dictionary.invalid_email[lan], 400)
+    return user_email
+
+##############################
+USER_USERNAME_MIN = 2
+USER_USERNAME_MAX = 20
+REGEX_USER_USERNAME = f"^.{{{USER_USERNAME_MIN},{USER_USERNAME_MAX}}}$"
+def validate_user_username():
+    user_username = request.form.get("user_username", "").strip()
+    error = f"username min {USER_USERNAME_MIN} max {USER_USERNAME_MAX} characters"
+    if len(user_username) < USER_USERNAME_MIN: raise Exception(error, 400)
+    if len(user_username) > USER_USERNAME_MAX: raise Exception(error, 400)
+    return user_username
+
+##############################
+USER_FIRST_NAME_MIN = 2
+USER_FIRST_NAME_MAX = 20
+REGEX_USER_FIRST_NAME = f"^.{{{USER_FIRST_NAME_MIN},{USER_FIRST_NAME_MAX}}}$"
+def validate_user_first_name():
+    user_first_name = request.form.get("user_first_name", "").strip()
+    error = f"first name min {USER_FIRST_NAME_MIN} max {USER_FIRST_NAME_MAX} characters"
+    if not re.match(REGEX_USER_FIRST_NAME, user_first_name): raise Exception(error, 400)
+    return user_first_name
+
+
+##############################
+USER_PASSWORD_MIN = 6
+USER_PASSWORD_MAX = 50
+REGEX_USER_PASSWORD = f"^.{{{USER_PASSWORD_MIN},{USER_PASSWORD_MAX}}}$"
+def validate_user_password(lan = "en"):
+    user_password = request.form.get("user_password", "").strip()
+    if not re.match(REGEX_USER_PASSWORD, user_password): raise Exception(dictionary.invalid_password[lan], 400)
+    return user_password
+
+
+
+
+##############################
+def validate_user_password_confirm():
+    user_password = request.form.get("user_password_confirm", "").strip()
+    if not re.match(REGEX_USER_PASSWORD, user_password): raise Exception("Twitter exception - Invalid confirm password", 400)
+    return user_password
+
+
+##############################
+REGEX_UUID4 = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
+def validate_uuid4(uuid4 = ""):
+    if not uuid4:
+        uuid4 = request.values.get("uuid4", "").strip()
+    if not re.match(REGEX_UUID4, uuid4): raise Exception("Twitter exception - Invalid uuid4", 400)
+    return uuid4
+
+
+##############################
+REGEX_UUID4_WITHOUT_DASHES = "^[0-9a-f]{8}[0-9a-f]{4}4[0-9a-f]{3}[89ab][0-9a-f]{3}[0-9a-f]{12}$"
+def validate_uuid4_without_dashes(uuid4 = ""):
+    error = "Invalid uuid4 without dashes"
+    if not uuid4: raise Exception(error, 400)
+    uuid4 = uuid4.strip()
+    if not re.match(REGEX_UUID4_WITHOUT_DASHES, uuid4): raise Exception(error, 400)
+    return uuid4
+
+##############################
+POST_MIN_LEN = 2
+POST_MAX_LEN = 250
+REGEX_POST = f"^.{{{POST_MIN_LEN},{POST_MAX_LEN}}}$"
+def validate_post(post = ""):
+    post = post.strip()
+    if not re.match(REGEX_POST, post): raise Exception("x-error post", 400)
+    return post
+
+
+##############################
+def send_email(to_email, subject, template):
+    try:
+        # Create a gmail fullflaskdemomail
+        # Enable (turn on) 2 step verification/factor in the google account manager
+        # Visit: https://myaccount.google.com/apppasswords
+        # Copy the key : pdru ctfd jdhk xxci
+
+        # Email and password of the sender's Gmail account
+        sender_email = "sophieteinvigkjer@gmail.com"
+        password = "eyha yoon tlhb fvim"  # If 2FA is on, use an App Password instead
+
+        # Receiver email address
+        receiver_email = to_email
+        
+        # Create the email message
+        message = MIMEMultipart()
+        message["From"] = "X clone"
+        message["To"] = to_email
+        message["Subject"] = subject
+
+        # Body of the email
+        message.attach(MIMEText(template, "html"))
+
+        # Connect to Gmail's SMTP server and send the email
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()  # Upgrade the connection to secure
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+        ic("Email sent successfully!")
+
+        return "email sent"
+       
+    except Exception as ex:
+        ic(ex)
+        raise Exception("cannot send email", 500)
+    finally:
+        pass
